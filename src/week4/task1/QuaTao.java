efae776383737c9ecec6c7b7e64842b34a500808
Student.java <37437557+nguyentienducc@users.noreply.github.com>
2018-10-10 10:14:53 +0700

    Update QuaTao.java

--- src/week4/task1/QuaTao.java
+++ src/week4/task1/QuaTao.java
@@ -1,4 +1,35 @@
-
-public class QuaTao {
-
+package week4.task1;
+public class QuaTao extends HoaQua{
+	private String xuatxu;
+	private int khoiluong;
+public QuaTao(){
+	this.xuatxu="";
+	this.khoiluong=0;
+}
+public void setLoaicam(String xuatxu ){
+	this.xuatxu=xuatxu;
+}
+public String getXuatxu(){
+	return xuatxu;
+}
+public void setKhoiluong(int khoiluong){
+	this.khoiluong=khoiluong;
+}
+public int getKhoiluong(){
+	return khoiluong;
+}
+public void congdungTao(){
+	System.out.println("cong dung cua cam la : ");
+	System.out.println("tot cho da va chong tram cam ");
+	System.out.println("chua chung thieu mau ");
+	System.out.println("giup xuong chac khoe");
+}
+public void kiemTra(Quatao t1, Quactao t2){
+	if(sosanhmausac(t1,t2)){
+		System.out.println("cung mot mau tao ");	
+	}
+	else{
+		System.out.println("khac mau tao ");
+	}
+}
 }
