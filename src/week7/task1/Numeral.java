7a7ffe668f65403cac21ed239f06e308f283adcc
Student.java <37437557+nguyentienducc@users.noreply.github.com>
2018-10-31 02:35:34 +0700

    Update Numeral.java

--- src/week7/task1/Numeral.java
+++ src/week7/task1/Numeral.java
@@ -1,4 +1,29 @@
 package week7.task1;
+public class Numeral extends Expression{
+    int value;
 
-public class Numeral {
+    public Numeral() {
+    }
+
+    public Numeral(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public int evaluate() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(value);
+    }
 }
