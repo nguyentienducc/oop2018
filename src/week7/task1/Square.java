a13e2a230fe61d8a54d0ea9f6ad0c5205d902f2b
Student.java <37437557+nguyentienducc@users.noreply.github.com>
2018-10-31 02:36:07 +0700

    Update Square.java

--- src/week7/task1/Square.java
+++ src/week7/task1/Square.java
@@ -1,4 +1,22 @@
 package week7.task1;
+public class Square extends Expression{
+    Expression expression;
 
-public class Square {
+    public Square(Expression expression) {
+        this.expression = expression;
+    }
+
+    @Override
+    public int evaluate(){
+        return expression.evaluate()*expression.evaluate();
+    }
+
+    @Override
+    public String toString() {
+        if(!(expression instanceof Numeral))
+            return '(' + expression.toString() +")^2";
+        else
+            return expression.toString() +"^2";
+
+    }
 }
