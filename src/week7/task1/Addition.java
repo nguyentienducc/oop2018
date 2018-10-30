08f24dc37d4eecb8536c06eeedc5b69b305418fb
Student.java <37437557+nguyentienducc@users.noreply.github.com>
2018-10-31 02:33:34 +0700

    Update Addition.java

--- src/week7/task1/Addition.java
+++ src/week7/task1/Addition.java
@@ -1,4 +1,32 @@
 package week7.task1;
+public class Addition extends BinaryExpression{
 
-public class Addition {
+    Expression left, right;
+
+    public Addition(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int evaluate() {
+        return (left.evaluate() + right.evaluate());
+    }
+
+    @Override
+    public Expression left() {
+
+
+        return left;
+    }
+
+    @Override
+    public Expression right() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return left.toString() + " + " + right.toString();
+    }
 }
