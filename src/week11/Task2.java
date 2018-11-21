2887d77bd17bc565abf2b8dd965c0a604cc1a801
Student.java <37437557+nguyentienducc@users.noreply.github.com>
2018-11-21 11:19:37 +0700

    lab11

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package week11;
-
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author Admin
+ */
 public class Task2 {
+    public static <T extends Comparable> T GetMax(ArrayList<T> arr)
+    {
+        T max = arr.get(0);
+        for(int i = 1 ; i < arr.size() ; i++) {
+            if(arr.get(i).compareTo(max) > 0)
+            {
+                max = arr.get(i);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> Arr=new ArrayList<>();
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        for(int i=0;i<num;i++)
+        {
+            Arr.add(scanner.nextInt());
+        }
+        System.out.println("GIÁ TRỊ LỚN NHẤT CỦA MẢNG LÀ : " +GetMax(Arr));
+    }
 }
